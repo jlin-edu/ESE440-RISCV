e13bde1d08cc9bfe49819c6afc338871c32c4dd9
jlin-edu <83518027+jlin-edu@users.noreply.github.com>
2024-10-22 22:43:11 -0400

    Update Immediate_Extend.sv

--- Immediate_Extend.sv
+++ Immediate_Extend.sv
@@ -4,7 +4,6 @@ module immediate_extender (
     input signed [`REG_RANGE] instruction,     
     input logic extend_flag,                       // Sign extend flag  
     input logic sign_extend[2:0],                       // Sign extend flag (3 bits for 4 types of sign extension)
-
     /*
     0: 12-bit immediate
     1: 13-bit immediate
@@ -17,9 +16,13 @@ module immediate_extender (
     store instructions
     */ 
     input signed [`IMM_RANGE_I] immediate,                  // Immediate value for I-type instructions
+    input logic byte_enable,                                // Byte enable flag from the control unit
+    input logic halfword_enable,                            // Halfword enable flag from the control unit
+    input logic word_enable,                                // Word enable flag from the control unit
     output logic [`REG_RANGE] result
     );
 
+// sign extension and re-arrangement of immediate value
 always_comb begin : Immediate_Extend_block
 if (extend_flag) begin
     case (sign_extend)
@@ -54,6 +57,16 @@ else begin
     result = immediate;
 end
 
+
+if (byte_enable) begin              // If byte enable is set, only the 8 LSBs are used
+    result = result[7:0];        
 end
-    
+else if (halfword_enable) begin     // If halfword enable is set, only the 16 LSBs are used
+    result = result[15:0];
+end
+else if (word_enable) begin         // If word enable is set, the 32 bits are used
+    result = result[31:0];
+end
+end
+end 
 endmodule
