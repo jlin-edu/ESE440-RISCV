78feee821ad0b97e149a95c993335f60cd6d3c1d
jlin-edu <jiajun.lin@stonybrook.edu>
2024-11-09 23:04:33 -0500

    Changes made, some logic were incorrect

--- Stages/instruction_decode.sv
+++ Stages/instruction_decode.sv
@@ -7,7 +7,12 @@ module inst_decode (
     output logic [`OP_RANGE] opcode,
     output logic [`FUNCT_3_RANGE] funct3,
     output logic [`FUNCT_7_RANGE] funct7,
-    output logic [`REG_RANGE] rs1, rs2, imm, debug_data,
+    output logic [`REG_RANGE] debug_data,
+    output logic [`REG_RS1] rs1,
+    output logic [`REG_RS2] rs2,
+    output logic [`REG_RD] rd,
+    output logic [`REG_RANGE] imm,
+    output logic reg_wr_en,
     output logic jump_sel, mem_we,
     output logic op1_sel, op2_sel
 );
@@ -16,21 +21,23 @@ logic [`REG_FIELD_RANGE] rs1_addr, rs2_addr, rd_addr;
 logic [1:0] reg_source;
 logic [`REG_RANGE] reg_data;
 logic reg_we;
+logic [`REG_RS1] rs1_decoder, rs1_regfile;
+logic [`REG_RS2] rs2_decoder, rs2_regfile;
 
-inst_decoder decoder (.inst(instruction), .rs1(rs1_addr), .rs2(rs2_addr), .rd(rd_addr),
+inst_decoder decoder (.inst(instruction), .rs1(rs1), .rs2(rs2), .rd(rd),
                     .op(opcode), .funct3(funct3), .funct7(funct7), .imm(imm));
 
 control_unit controller (.opcode(opcode), .funct3(funct3), .funct7(funct7),
                     .reg_wr_en(reg_we), .pc_rs1_sel(op1_sel),
                     .imm_rs2_sel(op2_sel), .jump_branch_sel(jump_sel), 
-                    .mem_write_enable(mem_we), .reg_write_ctrl(reg_source));
+                    .mem_wr_en(mem_we), .reg_write_ctrl(reg_source));
 
 RegFile #() registers (.clk(clk), .reset(reset), .write_enable(reg_we),
                     .read_addr1(rs1_addr), .read_addr2(rs2_addr), .write_addr(rd_addr),
-                    .write_data_in(reg_data), .read_data_out1(rs1), .read_data_out2(rs2),
+                    .write_data_in(reg_data), .read_data_out1(rs1_regfile), .read_data_out2(rs2_regfile),
                     .debug_en(debug_en), .debug_addr(debug_addr), .debug_data(debug_data));
 
-// TODO: MOVE TO WB IN FUTURE
+// TO DO: MOVE TO WB IN FUTURE
 always_comb begin
     reg_data = 0;
     case (reg_source)
@@ -39,5 +46,4 @@ always_comb begin
         2: reg_data = data_mem;
     endcase
 end
-
 endmodule
--- inst_decoder.sv
+++ inst_decoder.sv
@@ -2,7 +2,9 @@
 
 module inst_decoder (
     input [`REG_RANGE] inst,
-    output logic [`REG_FIELD_RANGE] rs1, rs2, rd,
+    output logic [`REG_RS1] rs1, 
+    output logic [`REG_RS2] rs2,
+    output logic [`REG_RD] rd,
     output logic [`OP_RANGE] op,
     output logic [`FUNCT_3_RANGE] funct3,
     output logic [`FUNCT_7_RANGE] funct7,
@@ -10,27 +12,65 @@ module inst_decoder (
 );
     always_comb begin
         op     = inst[`OP_FIELD];
-        rs1    = inst[`REG_RS1];
-        rs2    = inst[`REG_RS2];
-        rd     = inst[`REG_RD];
-        funct3 = inst[`FUNCT_3_FIELD];
-        funct7 = inst[`FUNCT_7_FIELD];
-
         imm = 0;
+        
         case (op)
-            `OP_LUI:    imm = 32'(inst[`IMM_FIELD_U] << 12);
-            `OP_AUIPC:  imm = 32'(inst[`IMM_FIELD_U] << 12);
-            `OP_JAL:    imm = 32'{inst[`IMM_FIELD_J_20], inst[`IMM_FIELD_J_19_12], inst[`IMM_FIELD_J_11], inst[`IMM_FIELD_J_10_1], 1'b0};
-            `OP_JALR:   imm = 32'(inst[`IMM_FIELD_I]);
-            `OP_BR:     imm = 32'(signed'{inst[`IMM_FIELD_B_12], inst[`IMM_FIELD_B_11], inst[`IMM_FIELD_B_10_5], inst[`IMM_FIELD_B_4_1], 1'b0});
-            `OP_LD:     imm = 32'(signed'(inst[`IMM_FIELD_I]));
-            `OP_ST:     imm = 32'(signed'{inst[`IMM_FIELD_S_U], inst[`IMM_FIELD_S_L]});
-            `OP_IMM: begin
-                if (funct3 == `ADDI || funct3 == `SLTI || funct3 == `ANDI || funct3 == `ORI || funct3 == `XORI) begin
+            `OP_R3:    begin
+                imm = 32'(0);
+                funct7 = inst[`FUNCT_7_FIELD];
+                funct3 = inst[`FUNCT_3_FIELD];
+                rd     = inst[`REG_RD];
+                rs1    = inst[`REG_RS1];
+                rs2    = inst[`REG_RS2];
+            end
+            `OP_LUI:    begin                   // U-type instruction
+                imm = 32'(inst[`IMM_FIELD_U] << 12);
+                rd     = inst[`REG_RD];
+            end 
+            `OP_AUIPC:  begin                   // U-type instruction
+                imm = 32'(inst[`IMM_FIELD_U] << 12);
+                rd     = inst[`REG_RD];
+            end 
+            `OP_JAL:    begin                   // J-type instruction
+                imm = 32'({inst[`IMM_FIELD_J_20], inst[`IMM_FIELD_J_10_1], inst[`IMM_FIELD_J_11], inst[`IMM_FIELD_J_19_12], 1'b0});
+                rd     = inst[`REG_RD];
+            end 
+            `OP_JALR:   begin                   // I-type instruction
+                imm = 32'(inst[`IMM_FIELD_I]);
+                rd     = inst[`REG_RD];
+                rs1    = inst[`REG_RS1];
+                funct3 = inst[`FUNCT_3_FIELD];
+            end 
+            `OP_BR:     begin                   // B-type instruction
+                imm = 32'(signed'({inst[`IMM_FIELD_B_12], inst[`IMM_FIELD_B_10_5],"0000000000000", inst[`IMM_FIELD_B_4_1], inst[`IMM_FIELD_B_11]}));
+                rs1    = inst[`REG_RS1];
+                rs2    = inst[`REG_RS2];
+                funct3 = inst[`FUNCT_3_FIELD];
+            end 
+            `OP_LD:     begin                   // I-type instruction
+                imm = 32'(signed'(inst[`IMM_FIELD_I]));
+                rd     = inst[`REG_RD];
+                rs1    = inst[`REG_RS1];
+                funct3 = inst[`FUNCT_3_FIELD];
+            end 
+            `OP_ST:     begin                   // S-type instruction
+                imm = 32'(signed'({inst[`IMM_FIELD_S_U], inst[`IMM_FIELD_S_L]}));
+                rs1    = inst[`REG_RS1];
+                rs2    = inst[`REG_RS2];
+                funct3 = inst[`FUNCT_3_FIELD];
+            end 
+            `OP_IMM: begin                      // I-type instruction
+                if (funct3 == `ADDI || funct3 == `SLTI || funct3 == `ANDI || funct3 == `ORI || funct3 == `XORI) begin       // Signed
                     imm = 32'(signed'(inst[`IMM_FIELD_I]));
+                    rd     = inst[`REG_RD];
+                    rs1    = inst[`REG_RS1];
+                    funct3 = inst[`FUNCT_3_FIELD];
                 end 
-                else begin
+                else begin          // Unsigned
                     imm = 32'(inst[`IMM_FIELD_I]);
+                    rd     = inst[`REG_RD];
+                    rs1    = inst[`REG_RS1];
+                    funct3 = inst[`FUNCT_3_FIELD];
                 end
             end
         endcase
