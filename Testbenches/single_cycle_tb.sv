5958bd14c77d9318ccaa1d9c40e334e58be4c9fe
Alec Merves <alec.merves@gmail.com>
2024-11-14 21:49:22 -0500

    Created single_cycle_tb

--- Testbenches/single_cycle_tb.sv
+++ Testbenches/single_cycle_tb.sv
@@ -13,37 +13,30 @@ module single_cycle_tb ();
     always #5 clk  = ~clk;
 
     single_cycle #(.WIDTH(WIDTH), .SIZE(SIZE)) dut(.clk(clk), .reset(reset),
-                                                    .instr_in(instr_in), .instr_wr_addr(instr_wr_addr), .instr_wr_en(instr_wr_en));
+                    .instr_in(instr_in), .instr_wr_addr(instr_wr_addr), .instr_wr_en(instr_wr_en));
 
-    logic [31:0] testData[20:0];
-    initial $readmemb("data.txt", testData);
+    int fd;
+    string line;
 
-    integer i;
-    initial begin
-        //@(posedge clk);
+    string program_file = "FILE NAME HERE";
+
+    initial begin    
+        fd = $fopen(program_file, "r");
+        instr_wr_addr = 0;
         reset = 1;
-        for(i=0; i<10; i=i+1) begin
-            instr_in = testData[i][31:0];     
-            instr_wr_addr = i*4;            //the i*4 is a left shift twice since we are indexing by word
-            instr_wr_en = 1;
+        instr_wr_en = 1;
+        while (!$feof(fd)) begin
+            $fgets(line, fd);
+            $sscanf(line, "%b\n", instr_in);
             @(posedge clk);
+            instr_wr_addr += 4;
         end
+        $fclose(fd);
+        instr_in = 0; instr_wr_addr = 0; instr_wr_en = 0;
+        reset = 0;
+        
+        for (int i = 0; i < 256; i++) begin @(posedge clk); end
 
-        #1; instr_in = 0; instr_wr_addr = 0; instr_wr_en = 0;
-        @(posedge clk);
-
-        #1; reset = 0;
-        @(posedge clk);
-
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
         $finish;
     end
 endmodule
