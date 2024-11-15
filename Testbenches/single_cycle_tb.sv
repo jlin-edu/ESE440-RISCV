e8bb996657f69728e6f4ff22ac05820a958f17bb
AlecMerves <99280412+AlecMerves@users.noreply.github.com>
2024-11-14 21:16:05 -0500

    Update single_cycle_tb.sv

--- Testbenches/single_cycle_tb.sv
+++ Testbenches/single_cycle_tb.sv
@@ -1,38 +1,49 @@
 `include "inst_defs.sv"
 
 module single_cycle_tb ();
+    parameter   WIDTH=32, SIZE=64;
+    localparam  LOGSIZE=$clog2(SIZE);
 
-logic clk, reset, debug_en, inst_we, inst_clk;
-logic [`REG_FIELD_RANGE] debug_reg_addr;
-logic [`REG_RANGE] debug_inst_addr, debug_mem_addr, inst_wr_addr, inst_wr_data;
-logic [`REG_RANGE] debug_inst_data, debug_reg_data, debug_mem_data;
+    logic [WIDTH-1:0] instr_in;
+    logic [(LOGSIZE-1)+2:0] instr_wr_addr;
+    logic instr_wr_en;
 
-single_cycle UUT (.*);
+    logic clk, reset;
+    initial clk  = 0;
+    always #5 clk  = ~clk;
 
-int in_fd, out_fd;
-string line;
+    single_cycle #(.WIDTH(WIDTH), .SIZE(SIZE)) dut(.clk(clk), .reset(reset),
+                                                    .instr_in(instr_in), .instr_wr_addr(instr_wr_addr), .instr_wr_en(instr_wr_en));
 
-initial begin
-    clk = 0;
-    reset = 0;
+    logic [31:0] testData[20:0];
+    initial $readmemb("data.txt", testData);
 
-    debug_en = 1;
-    debug_inst_addr = 0;
-    debug_mem_addr = 0;
-    debug_reg_addr = 0;
+    integer i;
+    initial begin
+        //@(posedge clk);
+        reset = 1;
+        for(i=0; i<10; i=i+1) begin
+            instr_in = testData[i][31:0];     
+            instr_wr_addr = i*4;            //the i*4 is a left shift twice since we are indexing by word
+            instr_wr_en = 1;
+            @(posedge clk);
+        end
 
-    inst_we = 0;
-    inst_clk = 0;
-    inst_wr_addr = 0;
-    inst_wr_data = 0;
-end
+        #1; instr_in = 0; instr_wr_addr = 0; instr_wr_en = 0;
+        @(posedge clk);
 
-always begin
-    if (clk_en) begin
-        #5 clk = ~clk;
+        #1; reset = 0;
+        @(posedge clk);
+
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        $finish;
     end
-end
-
-
-
 endmodule
