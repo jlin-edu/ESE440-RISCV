e39ac4e8c03cacffd56c43bcec181ffe5ec05909
Alec Merves <alec.merves@gmail.com>
2025-02-03 15:03:51 -0500

    Slight modifications to v0.01

--- gui/Frontend/GUI.py
+++ gui/Frontend/GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from turtle import title
 
 from Memory import Memory
 from Register import Register
@@ -8,9 +9,11 @@ from RegisterFile import RegisterFile
 class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title("Dire-WolVes RISC-V GUI v0.01")
         self.frame = ttk.Frame(self)
         self.load_widgets()
         self.grid_all()
+        self.resizable(False, False)
         self.mainloop()
     
     def load_widgets(self):
@@ -23,12 +26,25 @@ class GUI(tk.Tk):
         
         self.instruction_mem = Memory(self, 1024, "Instruction Memory")
         self.data_mem = Memory(self, 1024, "Data Memory")
+        
+        #TODO: flush out the controls
+        self.controls = ttk.Frame(self, relief="solid", borderwidth=1)
+        self.temp = ttk.Label(self.controls, text="Controls", anchor="center")
+        
+        self.stages = ttk.Frame(self, relief="solid", borderwidth=2)
+        self.temp2 = ttk.Label(self.stages, text="Pipeline Stages", anchor="center")
     
     def grid_all(self):
         self.pc.grid(col=0, row=0, sticky="w")
         self.registers.grid(column=0, row=1, rowspan=2)
         self.instruction_mem.grid(col=1, row=1)
         self.data_mem.grid(col=1, row=2)
+        
+        self.controls.grid(column=1, row=0, columnspan=2, sticky="nsew")
+        self.temp.grid(sticky="nsew")
+        
+        self.stages.grid(column=2, row=0, rowspan=3, sticky="nsew")
+        self.temp2.grid(sticky="nsew")
 
 if __name__ == "__main__":
     test = GUI()
