3e53d0e3626e96d860c144b1e5914aac3c87b521
Alec Merves <alec.merves@gmail.com>
2025-02-24 19:49:19 -0500

    Added a todo list

--- gui/main.py
+++ gui/main.py
@@ -1,6 +1,18 @@
 from lib.Backend.Controller import Controller
 from lib.Frontend.GUI import GUI
 
+"""
+TODO:
+- Pipeline status view
+- Change data view (hex, bin, dec, signed, unsigned)
+- Highlight value changes
+- Manually set time to go to + show current time
+- Run TCL commands
+- Change program file + display name (either through DPI or change SV file)
+- Mode button: single cycle vs pipeline
+"""
+
+
 if __name__ == "__main__":
     control = Controller()
     gui = GUI(control)
