17758f2183181545405476ed4219b039038acbd8
Alec Merves <alec.merves@gmail.com>
2025-04-01 16:17:58 -0400

    Finalized signed division as well as 32bit divisors

--- Testbenches/division_tb.sv
+++ Testbenches/division_tb.sv
@@ -2,8 +2,8 @@
 
 module division_tb;
 
-    logic [`REG_RANGE] dividend_tb, divisor_tb, quotient_tb, remainder_tb;
-    logic status_tb, reset_tb, clk_tb, signed_div_tb, start_tb, divide_by_zero_tb, overflow_tb;
+    logic signed [`REG_RANGE] dividend_tb, divisor_tb, quotient_tb, remainder_tb;
+    logic status_tb, reset_tb, clk_tb, signed_div_tb, start_tb, divide_by_zero_tb;
 
     division_wrapper UUT (
         .dividend(dividend_tb),
@@ -15,8 +15,7 @@ module division_tb;
         .clk(clk_tb),
         .signed_div(signed_div_tb),
         .start(start_tb),
-        .divide_by_zero(divide_by_zero_tb),
-        .overflow(overflow_tb)
+        .divide_by_zero(divide_by_zero_tb)
     );
 
     initial clk_tb = 0;
@@ -27,26 +26,30 @@ module division_tb;
         divisor_tb = 0;
         start_tb = 0;
         reset_tb = 0;
-        signed_div_tb = 0; // Set this to change un/signed division
+        signed_div_tb = 1;
 
         @(posedge clk_tb);
         reset_tb = 1;
         @(posedge clk_tb);
         reset_tb = 0;
 
-        for (longint i = 1; i < 2**32; i++) begin
-            for (int j = 1; j < 2**16 && j <= i; j++) begin
-                dividend_tb = i;
-                divisor_tb = j;
-                @(posedge clk_tb);
-                start_tb = 1;
-                @(posedge clk_tb);
-                start_tb = 0;
-                do @(posedge clk_tb); while (status_tb == 1);
-                if (quotient_tb != i / j)
-                    $display("ERROR: incorrect quotient for z = %0d, d = %0d. Got %0d, expected %0d", dividend_tb, divisor_tb, quotient_tb, i / j);
-                if (remainder_tb != i % j)
-                    $display("ERROR: incorrect remainder for z = %0d, d = %0d. Got %0d, expected %0d", dividend_tb, divisor_tb, remainder_tb, i % j);
+        for (longint i = 1; i < 2**31; i++) begin
+            for (longint j = 1; j < 2**31 && j <= i; j++) begin
+                for (int k = 1; k >= -1; k -= 2) begin
+                    for (int l = 1; l >= -1; l -= 2) begin
+                        dividend_tb = i*k;
+                        divisor_tb = j*l;
+                        @(posedge clk_tb);
+                        start_tb = 1;
+                        @(posedge clk_tb);
+                        start_tb = 0;
+                        do @(posedge clk_tb); while (status_tb == 1);
+                        if (quotient_tb != (i*k) / (j*l))
+                            $display("ERROR: incorrect quotient for z = %0d, d = %0d. Got %0d, expected %0d", dividend_tb, divisor_tb, quotient_tb, (i*k) / (j*l));
+                        if (remainder_tb != (i*k) % (j*l))
+                            $display("ERROR: incorrect remainder for z = %0d, d = %0d. Got %0d, expected %0d", dividend_tb, divisor_tb, remainder_tb, (i*k) % (j*l));
+                    end
+                end
             end
         end
         $finish;
