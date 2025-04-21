b50047f61cb7f8c00cecdd5bb12d6355748e5aaa
AlecMerves <99280412+AlecMerves@users.noreply.github.com>
2025-04-21 16:54:16 -0400

    Added send instructions code

--- send_uart.py
+++ send_uart.py
@@ -10,9 +10,17 @@ except serial.SerialException as e:
     print(f"Error opening serial port: {e}")
     exit()
 
+"""userProgram = input("Enter the program to use:")
+with open(userProgram, 'r') as program:
+    lines = program.readlines()
+    num_lines = len(lines)
+    data = num_lines.to_bytes(2).decode()
+    ser.write(data.encode())
+    for line in lines:
+        data = "".join([int(line[8*i:8*(i+1)], 2).to_bytes(1).decode() for i in range(4)])
+        ser.write(data.encode())"""
 
-data = "Hello world"
-
+data = "hello world"
 try:
     ser.write(data.encode())
     print(f"Sent: {data}")
