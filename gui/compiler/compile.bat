1f315ed918fa28fe9e6187fa0c4a092822b74d84
Alec Merves <alec.merves@gmail.com>
2025-05-09 16:54:00 -0400

    Renamed folders

--- gui/compiler/compile.bat
+++ gui/compiler/compile.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p program="Enter C file to compile: "
 @echo on
-riscv64-unknown-elf-gcc -c -isystem %~dp0 -march=rv32im -mabi=ilp32 -ffreestanding %program%.c -o %program%.o
+riscv64-unknown-elf-gcc -c -include %~dp0start.h -march=rv32im -mabi=ilp32 -ffreestanding %program%.c -o %program%.o
 riscv64-unknown-elf-gcc -T %~dp0linker.ld -march=rv32im -mabi=ilp32 -nostdlib %program%.o -o %program%.elf
 riscv64-unknown-elf-objcopy -O binary %program%.elf %program%.bin
